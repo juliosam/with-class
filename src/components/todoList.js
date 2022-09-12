21cbc83d0ae42251912aa811166cf1e9911c9600
Julio <ja.samaniegom@gmail.com>
2022-09-12 11:34:05 -0500

    todo list working

--- src/components/todoList.js
+++ src/components/todoList.js
@@ -8,26 +8,58 @@ export default class TodoList extends Component {
       taskList : []
     };
   }
-  writing(e){
-    this.setState.oninput(e.target.value)
-  }
-  addTask(e){
-    e.preventDefault()
-    this.setState.taskList([...this.state.taskList, this.state.oninput])
-    this.setState.oninput("")
+
+  //method to update the input content, called onchange
+  writing(key,value){
+    this.setState({
+      [key]: value
+    })
+    console.log(this.state)
   }
 
+  //method to add a new item to de todo array
+  addTask(e){
+    e.preventDefault()
+    
+    const oninput = {
+      id: 1 + Math.random(),
+      value: this.state.oninput.slice()
+    }
+    
+    const taskList = [...this.state.taskList];
+
+    taskList.push(oninput);
+
+    this.setState({
+      taskList,
+      oninput:""
+    })
+  }
+  
   render() {
     return (
-      <form className='todo-list' onSubmit={this.addTask}>
-        <label>Write task</label>
-        <input
-          type="text"
-          value={this.state.oninput}
-          onChange={this.writing}
-        />
-        <button>Add</button>
-      </form>
+      <div>
+        <form className='todo-list' onSubmit={e => this.addTask(e)}>
+          <label>Write task</label>
+          <input
+            type="text"
+            value={this.state.oninput}
+            onChange={e => this.writing("oninput", e.target.value)}
+          />
+          <button>Add</button>
+        </form>
+        <ul>
+          {
+            this.state.taskList.map(item => {
+              return(
+                <li key={item.id}>
+                  {item.value}
+                </li>
+              )
+            })
+          }
+        </ul>
+      </div>
     )
   }
 }
